835f69a771c8c18842747e3b06d2a22f0c9e4ad5
Brian O'Connor <briandoconnor@gmail.com>
2017-05-31 13:05:38 +0200

    update the download script

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -1,1 +1,7 @@
-# todo
+#!/bin/bash
+
+# downloads data files too big to store on github
+
+# smartseq2
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR163/003/ERR1630013/ERR1630013.fastq.gz -O smartseq2/23bd7eb0-23a3-4898-b043-f7e982de281f.v1/ERR1630013.fastq.gz
