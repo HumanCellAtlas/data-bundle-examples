97b1c7d2be72fee534e7842774bf8d36a31b26dc
Brian O'Connor <briandoconnor@gmail.com>
2017-06-19 18:47:42 -0700

    need to test

--- bin/get_import_data.py
+++ bin/get_import_data.py
@@ -37,6 +37,7 @@ class GetImportData:
         args = parser.parse_args()
         self.input_dir = args.input_dir
         self.output_s3_dir = args.output_s3_dir
+        self.conn = boto.connect_s3()
 
         # run
         self.run()
@@ -61,10 +62,13 @@ class GetImportData:
             ctx.check_hostname = False
             ctx.verify_mode = ssl.CERT_NONE
             try:
-                print("DOWNLOADING: "+str(dir+"/"+name)+" TO: "+directory+"/"+name)
                 if (self.source_newer_or_diff_size(str(dir+"/"+name), self.output_s3_dir+"/"+directory+"/"+name)):
-                    #urlretrieve(str(dir+"/"+name), directory+"/"+name)
+                    print("DOWNLOADING: "+str(dir+"/"+name)+" TO: "+directory+"/"+name)
+                    urlretrieve(str(dir+"/"+name), directory+"/"+name)
                     self.upload(directory+"/"+name)
+                    # TODO: delete file here
+                else:
+                    print("SKIPPING DOWNLOAD: "+str(dir+"/"+name)+" TO: "+directory+"/"+name+" FILE SIZES IDENTICAL")
             except Exception as error:
                 print ("ERROR: "+error)
                 # just exit with non-zero status
@@ -72,19 +76,22 @@ class GetImportData:
 
     def upload(self, path):
         print("UPLOADING: "+path+" to "+self.output_s3_dir+"/"+path)
+        (bucket, key) = self.parse_bucket_key(self.output_s3_dir+"/"+path)
+        file = open(self.output_s3_dir+"/"+path, 'r+')
+        if (self.upload_to_s3(file, bucket, key)):
+            print("FINISHED!")
+        else:
+            print("FAILED TO UPLOAD")
 
     def source_newer_or_diff_size(self, web_source, s3_destination):
         site = urlopen(web_source)
         meta = site.info()
-        print (meta)
+        #print (meta)
         web_size = meta.get("Content-Length")
         print ("Content-Length:", meta.get("Content-Length"))
-        conn = boto.connect_s3()
-        m = re.search('^s3://([^/]+)/(\S+)$', s3_destination)
-        s3_bucket = m.group(1)
-        s3_key = m.group(2)
+        (s3_bucket, s3_key) = self.parse_bucket_key(s3_destination)
         print ("S3Bucket: "+s3_bucket+" S3Key: "+s3_key)
-        bk = conn.get_bucket(s3_bucket)
+        bk = self.conn.get_bucket(s3_bucket)
         key = bk.lookup(s3_key)
         if (key == None):
             print ("S3 key doesn't exist")
@@ -93,6 +100,52 @@ class GetImportData:
             print ("S3 Size: "+key.size)
         s3_size = key.size
         return(s3_size != web_size)
+
+    def parse_bucket_key(self, s3_path):
+        m = re.search('^s3://([^/]+)/(\S+)$', s3_path)
+        s3_bucket = m.group(1)
+        s3_key = m.group(2)
+        return(s3_bucket, s3_key)
+
+    def upload_to_s3(self, file, bucket, key, callback=None, md5=None, reduced_redundancy=False, content_type=None):
+        """
+        see http://stackabuse.com/example-upload-a-file-to-aws-s3/
+        Uploads the given file to the AWS S3
+        bucket and key specified.
+
+        callback is a function of the form:
+
+        def callback(complete, total)
+
+        The callback should accept two integer parameters,
+        the first representing the number of bytes that
+        have been successfully transmitted to S3 and the
+        second representing the size of the to be transmitted
+        object.
+
+        Returns boolean indicating success/failure of upload.
+        """
+        try:
+            size = os.fstat(file.fileno()).st_size
+        except:
+            # Not all file objects implement fileno(),
+            # so we fall back on this
+            file.seek(0, os.SEEK_END)
+            size = file.tell()
+
+        bucket = self.conn.get_bucket(bucket, validate=True)
+        k = Key(bucket)
+        k.key = key
+        if content_type:
+            k.set_metadata('Content-Type', content_type)
+        sent = k.set_contents_from_file(file, cb=callback, md5=md5, reduced_redundancy=reduced_redundancy, rewind=True)
+
+        # Rewind for later use
+        file.seek(0)
+
+        if sent == size:
+            return True
+        return False
 
 # run the class
 if __name__ == '__main__':
