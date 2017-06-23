80433fbb3494464d94f10affcaeb8191cdefbd89
Brian O'Connor <briandoconnor@gmail.com>
2017-06-22 23:34:16 -0700

    updates to work on tagging

--- bin/s3_examples/s3_example_bundle.py
+++ bin/s3_examples/s3_example_bundle.py
@@ -10,8 +10,8 @@ class S3ExampleBundle:
     BUNDLE_EXAMPLES_BUNDLE_LIST_PATH = "import/bundle_list"
 
     def __init__(self, bucket, root, bundle_path):
-        s3 = boto3.resource('s3')
-        self.bucket = s3.Bucket(bucket)
+        self.s3 = boto3.resource('s3')
+        self.bucket = self.s3.Bucket(bucket)
         self.bucket_str = bucket
         self.root = root
         self.path = bundle_path
